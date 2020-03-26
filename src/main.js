412ad70109479757b4cce1d474f67b822cc2d7f8
cheap-glitch <cheap.glitch@gmail.com>
2020-03-26 18:03:23 +0100

    Fix deployment error

--- src/main.js
+++ src/main.js
@@ -57,8 +57,6 @@ const { wrapHTML           } = require('./helpers.js');
 const files                  = require('./files.js');
 const fixes                  = require('./fixes.js');
 
-const PRODUCTION             = process.env.NODE_ENV === 'production';
-const TARGET                 = process.argv[2]
 const TIMESTAMP              = Date.now();
 const BUILD_DIR              = path.resolve(__dirname, '../public');
 const HTML_LAYOUT            = fs.readFileSync(path.resolve(__dirname, './layouts/page.html')).toString();
@@ -66,16 +64,14 @@ const HTML_LAYOUT            = fs.readFileSync(path.resolve(__dirname, './layout
 // Do nothing if no argument is given
 if (process.argv.length <= 2) process.exit(0);
 
-// Create 'public/' if it doesn't exist
+// Create the build directory if it doesn't exist
 if (!fs.existsSync(BUILD_DIR)) fs.mkdirSync(BUILD_DIR);
 
-/**
- * Build
- * =============================================================================
- */
-
-switch (TARGET)
+switch (process.argv[2])
 {
+	/**
+	 * Build HTML pages from the raw help files
+	 */
 	case 'html':
 		for (const [filename, title] of Object.entries(files))
 		{
@@ -101,16 +97,9 @@ switch (TARGET)
 		}
 		break;
 
-	case 'css':
-		rm.sync(`${BUILD_DIR}/*.css`);
-		walk(path.resolve(__dirname,  './styles')).forEach(file => fs.copyFileSync(file.path, `${BUILD_DIR}/${basename(file.path)}-${TIMESTAMP}.css`));
-		break;
-
-	case 'js':
-		rm.sync(`${BUILD_DIR}/*.js`);
-		walk(path.resolve(__dirname, './scripts')).forEach(file => fs.copyFileSync(file.path, `${BUILD_DIR}/${basename(file.path)}-${TIMESTAMP}.js`));
-		break;
-
+	/**
+	 * Build HTML pages from the Markdown files
+	 */
 	case 'static':
 		const gm = require('gray-matter');
 		const mi = require('markdown-it')({
@@ -141,65 +130,33 @@ switch (TARGET)
 			});
 		});
 		break;
+
+	/**
+	 * Update the stylesheets
+	 */
+	case 'css':
+		rm.sync(`${BUILD_DIR}/*.css`);
+		walk(path.resolve(__dirname,  './styles')).forEach(file => fs.copyFileSync(file.path, `${BUILD_DIR}/${basename(file.path)}-${TIMESTAMP}.css`));
+		break;
+
+	/**
+	 * Update the scripts
+	 */
+	case 'js':
+		rm.sync(`${BUILD_DIR}/*.js`);
+		walk(path.resolve(__dirname, './scripts')).forEach(file => fs.copyFileSync(file.path, `${BUILD_DIR}/${basename(file.path)}-${TIMESTAMP}.js`));
+		break;
 }
 
-/**
- * Timestamps
- * =============================================================================
- */
-
+// Update the timestamps
 walk(BUILD_DIR).forEach(function(file)
 {
-	// Update the timestamps in the source code
+	// In the source code
 	fs.writeFileSync(file.path, fs.readFileSync(file.path).toString().replace(/-\d{13}/g, `-${TIMESTAMP}`));
 
-	// Update the timestamp in the filename
+	// In the filenames
 	if (/\.(?:css|js)$/.test(file.path))
 		fs.renameSync(file.path, file.path.replace(/-\d{13}/, `-${TIMESTAMP}`));
-});
-
-/**
- * Post-processing
- * =============================================================================
- */
-
-if (PRODUCTION) walk(BUILD_DIR).filter(file => file.path.split('.').pop() == TARGET).forEach(async function(file)
-{
-	const minify       = require('html-minifier').minify;
-	const terser       = require('terser');
-	const postcss      = require('postcss');
-	const autoprefixer = require('autoprefixer');
-	const cssnano      = require('cssnano');
-
-	const source = fs.readFileSync(file.path).toString();
-
-	switch(TARGET)
-	{
-		// Minify the HTML
-		case 'html':
-			try {
-				fs.writeFileSync(file.path, minify(source, {
-					minifyJS:           true,
-					removeComments:     true,
-					collapseWhitespace: true,
-				}));
-			}
-			catch (error) {
-				console.info(`${file.path}`);
-				console.error(error);
-			}
-			break;
-
-		// Minify and auto-prefix the CSS
-		case 'css':
-			fs.writeFileSync(file.path, await postcss([autoprefixer, cssnano]).process(source));
-			break;
-
-		// Minify the JS
-		case 'js':
-			fs.writeFileSync(file.path, terser.minify(source).code);
-			break;
-	}
 });
 
 /**
